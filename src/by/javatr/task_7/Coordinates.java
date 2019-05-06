88b12e5a92333cf8df3d9111ed97bee292ab943d
Pavel <43853967+biyorn@users.noreply.github.com>
2019-05-06 23:22:24 +0300

    Monday

--- src/by/javatr/task_7/Coordinates.java
+++ src/by/javatr/task_7/Coordinates.java
@@ -1,8 +1,11 @@
 package by.javatr.task_7;
 
+import java.util.Map;
+
 public class Coordinates {
 
-    public static void m() {
+    public static void m(int pointAX, int pointAY, int pointBX, int pointBY) {
+
 
     }
 }
